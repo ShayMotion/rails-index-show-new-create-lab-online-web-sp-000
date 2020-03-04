d4b77167f842d884add527d566f13abee8fe869d
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-03-04 16:22:13 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,8 +5,9 @@ class CouponsController < ApplicationController
 end
 
   def create
-     @coupon = Coupon.find(params[:id])
-end
+        coupon = Coupon.create(store: params[:store], coupon_code: params[:coupon_code])
+        redirect_to coupon_path(coupon)
+    end
 
   def new 
 end
